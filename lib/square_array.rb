e61010b741f4a17ad9d5ef1449f3075731255e89
Misel Evtimov <misel.evtimov@gmail.com>
2020-12-16 08:10:51 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,10 @@
 def square_array(array)
   new_numbers = []
-  brojilo = 0
-  while brojilo < array.length
-    new_numbers.push(array[brojilo] **2)
-    brojilo += 1
+  counter = 0
+  while counter < array.length
+    new_numbers.push(array[counter] **2)
+    counter += 1
   end
   return new_numbers
 end
+
