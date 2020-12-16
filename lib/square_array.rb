655d48e9352fc8d73a283266244180a0d2b04b8f
Misel Evtimov <misel.evtimov@gmail.com>
2020-12-16 07:48:44 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,9 @@
 def square_array(array)
-  array = [1, 2, 3]
-  count = 0
-  while count < array.length do
-    array.index ** 2
+  new_numbers = []
+  counter = 0
+  while counter < array.length()
+    new_numbers.push(array[counter] * array[counter])
+    counter += 1
   end
-  square_array(array)
+  return new_numbers
 end
