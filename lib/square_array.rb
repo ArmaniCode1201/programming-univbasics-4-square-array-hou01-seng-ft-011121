e2405f009f20ed2217c09542fe4be6fd166443cc
Misel Evtimov <misel.evtimov@gmail.com>
2020-12-16 07:35:16 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   array = [1, 2, 3]
   count = 0
   while count < array.length do
-    square_array(array).index ** 2
+    array.index ** 2
   end
   square_array(array)
 end
